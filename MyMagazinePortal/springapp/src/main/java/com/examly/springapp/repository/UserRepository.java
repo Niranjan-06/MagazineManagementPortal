f414bb94dfbc0bf15ecd35c409cc8a5e43d2a68d
Niranjan-06 <727723eucs149@skcet.ac.in>
2025-03-30 21:54:31 +0530

    Update UserRepository.java

--- MyMagazinePortal/springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ MyMagazinePortal/springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -1,3 +1,4 @@
+
 package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +7,9 @@ import com.examly.springapp.model.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
 
+           @Query("SELECT o FROM User o WHERE o.id = :id")
+    Optional<User>findByEmail(String email);
+
+      @Query("SELECT i FROM User i WHERE i.password = :password")
+    List<User>findUserByPassword(@Param("password") String password);
 }
